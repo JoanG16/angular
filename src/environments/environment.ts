1feda939cd582253a26c29a00fead216be30b057
JoanG16 <e1313197400@live.uleam.edu.ec>
2025-07-22 01:33:41 -0500

    cambios de enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.development.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/v1/api' // Mantén esta línea para desarrollo local
+  apiUrl: 'https://back-base-project-main.onrender.com/v1/api' // Mantén esta línea para desarrollo local
 };
